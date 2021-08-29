87d0b199b9311abf0b6ab7d6bc428b8109b377f8
Mac Ignacio <mack_ignacio@yahoo.com>
2021-08-29 16:48:50 +0800

    Update 51.js test

--- test/51.js
+++ test/51.js
@@ -1,11 +1,19 @@
 const { parse } = require('../dist');
 
-describe.skip('issue 51', function () {
+describe('issue 51', function () {
 	it('vue: > in attibute value', function () {
-		const html = '<template v-if="list.length>0"> <div> 123 </div> </template>';
+		// Reference https://github.com/taoqf/node-html-parser/issues/51#issue-636325007
+		const html = '<template v-if="list.length>0"> <div>123</div> </template>';
 		const root = parse(html);
 		root.toString().should.eql(html);
 		const tpl = root.firstChild;
+
 		tpl.getAttribute('v-if').should.eql('list.length>0');
+		should.equal(tpl.rawAttrs, 'v-if="list.length>0"');
+		should.equal(tpl.tagName, 'TEMPLATE');
+
+		const div = tpl.childNodes[1];
+		div.textContent.should.eql('123');
+		console.log(div.textContent);
 	});
 });
