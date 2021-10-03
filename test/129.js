9a9226c7ed6611cb04f537a64c8e6d55fe983d85
Ron S <ron@nonara.com>
2021-10-03 15:44:18 -0400

    test: Added test for prototype pollution (closes #129)

---
+++ test/129.js
@@ -0,0 +1,10 @@
+const { parse } = require('../dist');
+
+// see: https://github.com/taoqf/node-html-parser/issues/129
+describe('Prototype pollution', () => {
+  it('prevents prototype pollution',  () => {
+    const root = parse('<a href="#" __proto__="polluted=true">');
+    should(root.firstChild.attributes.polluted).not.be.ok();
+    should(root.firstChild.attributes.hasOwnProperty('proto__')).be.ok();
+  });
+});
